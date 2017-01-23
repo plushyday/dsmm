ef9ddb24587936781e0a458c4296261380ecbce3
Anastasia Malakhova <plushik.n@gmail.com>
2017-01-23 14:56:56 +0300

    slider

---
+++ slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+var slider_item = $('.slider').children();
+	for (var i = 0; i < slider_item.length; ++i) {
+		for(var j=0; j < )
+		var offset += 10;
+		do slider_item[i].css({
+			"margin-left": 'value1',
+			property2: 'value2'
+		});
+	};
+
+});
